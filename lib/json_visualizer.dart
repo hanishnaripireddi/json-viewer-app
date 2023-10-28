6b78ea408b8af4514336291784c2c63b12e351c1
hanishnaripireddi <hanishnaripireddi@gmail.com>
2023-10-28 18:14:12 +0530

    basic ui 1

--- lib/json_visualizer.dart
+++ lib/json_visualizer.dart
@@ -1,9 +1,9 @@
-
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 
 class JSONVisualizer extends StatefulWidget {
+  const JSONVisualizer({super.key});
+
   @override
   _JSONVisualizerState createState() => _JSONVisualizerState();
 }
@@ -60,28 +60,40 @@ class _JSONVisualizerState extends State<JSONVisualizer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                maxLines: null, // Allow multiple lines of input
-                decoration: InputDecoration(labelText: 'Enter JSON Data'),
-                onChanged: (value) {
-                  setState(() {
-                    jsonInput = value;
-                    parseJSON();
-                  });
-                },
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                height: 300, // Set the height to 300 pixels
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  maxLines: null, // Allow multiple lines of input
+                  decoration: InputDecoration(labelText: 'Enter JSON Data',border: InputBorder.none),
+                  onChanged: (value) {
+                    setState(() {
+                      jsonInput = value;
+                      parseJSON();
+                    });
+                  },
+                ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: buildNestedJson(jsonData),
-            ),
-          ],
-        ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: buildNestedJson(jsonData),
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:json_viewer/json_visualizer.dart';
 
